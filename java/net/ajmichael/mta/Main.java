3d344733bcd524d2d897878b979dede7ba0c91e5
Adam Michael <adam@ajmichael.net>
2017-02-19 22:53:14 -0500

    Made the application query for all of the next L train arrivals at Bedford and print to stdout.

--- java/net/ajmichael/mta/Main.java
+++ java/net/ajmichael/mta/Main.java
@@ -1,35 +1,82 @@
 package net.ajmichael.mta;
 
-import com.google.transit.realtime.NyctSubway;
+import com.google.transit.realtime.GtfsRealtime;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.stream.Collectors;
 
+// L1 is 8th ave
+// L6 is Bedford
+// L17 is Myrtle-Wyckoff
+// L29 is Rockaway-Canarsie
+// South trains are Brooklyn-bound
 public final class Main {
-  private static final String BASE_FEED_URL = "http://datamine.mta.info/mta_esi.php?key=";
   private static final String CONFIG_FILE = "config.properties";
-  private static final Properties properties = new Properties();
+  private static final String MANHATTAN_BOUND_BEDFORD = "L06N";
+  private static final URL L_TRAIN_FEED_URL;
+  private static final long TIMER_DELAY_MILLIS = 1000;
+  private static final long TIMER_PERIOD_MILLIS = 5000;
 
   static {
     ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
     try (InputStream configStream = contextClassLoader.getResourceAsStream(CONFIG_FILE)) {
+      Properties properties = new Properties();
       properties.load(configStream);
+      L_TRAIN_FEED_URL =
+          new URL(
+              String.format(
+                  "http://datamine.mta.info/mta_esi.php?feed_id=2&key=%s",
+                  properties.getProperty("mtaapikey")));
     } catch (IOException e) {
-      throw new RuntimeException("Failed to load properties file", e);
+      throw new RuntimeException("Failed to initialize Main", e);
     }
   }
 
   public static void main(String[] args) throws Exception {
-    URL url = new URL(BASE_FEED_URL + properties.getProperty("mtaapikey"));
-    NyctSubway.NyctFeedHeader nyctFeedHeader =
-        NyctSubway.NyctFeedHeader.parseFrom(url.openStream());
-    System.out.println(nyctFeedHeader);
-    List<NyctSubway.TripReplacementPeriod> tripReplacementPeriods =
-        nyctFeedHeader.getTripReplacementPeriodList();
-    for (NyctSubway.TripReplacementPeriod tripReplacementPeriod : tripReplacementPeriods) {
-      System.out.println(tripReplacementPeriod.getRouteId());
+    new Timer()
+        .scheduleAtFixedRate(
+            new FetchTask(L_TRAIN_FEED_URL), TIMER_DELAY_MILLIS, TIMER_PERIOD_MILLIS);
+  }
+
+  private static class FetchTask extends TimerTask {
+    private final URL url;
+
+    private FetchTask(URL url) {
+      this.url = url;
+    }
+
+    @Override
+    public void run() {
+      try {
+        List<Long> minutesTilNextTrains =
+            GtfsRealtime.FeedMessage.parseFrom(url.openStream())
+                .getEntityList()
+                .stream()
+                .map(GtfsRealtime.FeedEntity::getTripUpdate)
+                .map(GtfsRealtime.TripUpdate::getStopTimeUpdateList)
+                .filter(stopTimeUpdates -> !stopTimeUpdates.isEmpty())
+                .flatMap(Collection::stream)
+                .filter(
+                    stopTimeUpdate -> stopTimeUpdate.getStopId().equals(MANHATTAN_BOUND_BEDFORD))
+                .map(GtfsRealtime.TripUpdate.StopTimeUpdate::getArrival)
+                .map(GtfsRealtime.TripUpdate.StopTimeEvent::getTime)
+                .map(Instant::ofEpochSecond)
+                .map(i -> Instant.now().until(i, ChronoUnit.MINUTES))
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("Next L trains at Bedford Ave");
+        System.out.println(minutesTilNextTrains);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
